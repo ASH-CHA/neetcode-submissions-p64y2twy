2e7d07b1c30c47e9043e206ac676fb0d0f769962
ASH-CHA <ashton15chavez@gmail.com>
2026-04-09 06:04:52 +0200

    Add: is-palindrome - submission-6

---
+++ Data Structures & Algorithms/is-palindrome/submission-6.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(String s) {
+        int left = 0, right = s.length() - 1;
+
+        while(left < right){
+            while(left < right && !alphaNum(s.charAt(left))) left++;
+            while(left < right && !alphaNum(s.charAt(right))) right--;
+
+            if(Character.toLowerCase(s.charAt(left)) != Character.toLowerCase(s.charAt(right))){
+                return false;
+            }
+
+            left++;
+            right--;
+        }
+        return true;
+    }
+
+    private boolean alphaNum(char c){
+        return (c >= 'a' && c <= 'z') ||
+               (c >= 'A' && c <= 'Z') ||
+               (c >= '0' && c <= '9');
+    }
+}
