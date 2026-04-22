c382afcc54d9269e34ead9b83196f3aa79c74a91
ASH-CHA <ashton15chavez@gmail.com>
2026-04-22 11:07:09 +0200

    Add: jump-game-ii - submission-2

---
+++ Data Structures & Algorithms/jump-game-ii/submission-2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int jump(int[] nums) {
+        int jumps = 0;
+        int currMax = 0, currEnd = 0;
+
+        for(int i = 0; i < nums.length - 1; i++){
+            currMax = Math.max(currMax, i + nums[i]);
+
+            if(i == currEnd){
+                jumps++;
+                currEnd = currMax;
+            }
+        }
+        return jumps;
+    }
+}
