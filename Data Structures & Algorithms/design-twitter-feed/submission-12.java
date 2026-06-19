ee8ec3e700648800f9cc31ba9973cc4a23ba13c6
ASH-CHA <ashton15chavez@gmail.com>
2026-06-19 05:25:29 +0200

    Add: design-twitter-feed - submission-12

---
+++ Data Structures & Algorithms/design-twitter-feed/submission-12.java
@@ -0,0 +1,60 @@
+class Twitter {
+
+    private int time;
+    private Map<Integer, Set<Integer>> followMap;
+    private Map<Integer, List<Tweet>> tweetMap;
+
+    class Tweet{
+        int id;
+        int time;
+
+        public Tweet(int id, int time){
+            this.id = id;
+            this.time = time;
+        }
+    }
+
+    public Twitter() {
+        time = 0;
+        followMap = new HashMap<>();
+        tweetMap = new HashMap<>();
+    }
+    
+    public void postTweet(int userId, int tweetId) {
+        tweetMap.putIfAbsent(userId, new ArrayList<>());
+        tweetMap.get(userId).add(new Tweet(tweetId, time++));
+    }
+    
+    public List<Integer> getNewsFeed(int userId) {
+        PriorityQueue<Tweet> pq = new PriorityQueue<>((a, b) -> b.time - a.time);
+
+        followMap.putIfAbsent(userId, new HashSet<>());
+        followMap.get(userId).add(userId);
+
+        for(int followee : followMap.get(userId)){
+            List<Tweet> tweets = tweetMap.getOrDefault(followee, new ArrayList<>());
+
+            for(Tweet t : tweets) pq.offer(t);
+        }
+
+        List<Integer> result = new ArrayList<>();
+        int count = 0;
+
+        while(!pq.isEmpty() && count < 10){
+            result.add(pq.poll().id);
+            count++;
+        }
+        return result;
+    }
+    
+    public void follow(int followerId, int followeeId) {
+        followMap.put(followerId, new HashSet<>());
+        followMap.get(followerId).add(followeeId);
+    }
+    
+    public void unfollow(int followerId, int followeeId) {
+        if(followMap.containsKey(followerId)){
+            followMap.get(followerId).remove(followeeId);
+        }
+    }
+}
