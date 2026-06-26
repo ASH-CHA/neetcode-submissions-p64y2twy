da2b3015ceb68b281988cb2df4aa7039af8ac69d
ASH-CHA <ashton15chavez@gmail.com>
2026-06-26 07:00:29 +0200

    Add: partition-labels - submission-1

---
+++ Data Structures & Algorithms/partition-labels/submission-1.java
@@ -0,0 +1,21 @@
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        int[] lastOccurence = new int[26];
+        for(int i = 0; i < s.length(); i++){
+            lastOccurence[s.charAt(i) - 'a'] = i;
+        }
+
+        List<Integer> ans = new ArrayList<>();
+        int partitionEnd = 0, partitionStart = 0;
+
+        for(int i = 0; i < s.length(); i++){
+            partitionEnd = Math.max(partitionEnd, lastOccurence[s.charAt(i) - 'a']);
+
+            if(i == partitionEnd){
+                ans.add(partitionEnd - partitionStart + 1);
+                partitionStart = i + 1;
+            }
+        }
+        return ans;
+    }
+}
