a5c86dd1312b99eb69bda19640e132373e9be255
ASH-CHA <ashton15chavez@gmail.com>
2026-07-01 07:23:19 +0200

    Add: reverse-integer - submission-6

---
+++ Data Structures & Algorithms/reverse-integer/submission-6.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int reverse(int x) {
+        int ans = 0;
+        boolean isNegative = x < 0;
+        String strX = String.valueOf(Math.abs(x));
+        StringBuilder sb = new StringBuilder(strX).reverse();
+
+        try{
+            ans = Integer.parseInt(sb.toString());
+        } catch(NumberFormatException e){
+            return 0;
+        }
+        return isNegative ? -ans : ans;
+    }
+}
