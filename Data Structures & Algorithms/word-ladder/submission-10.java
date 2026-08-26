ce5f56c48f715fa01971f8de33d88f539692a440
ASH-CHA <ashton15chavez@gmail.com>
2026-08-26 09:30:36 +0200

    Add: word-ladder - submission-10

---
+++ Data Structures & Algorithms/word-ladder/submission-10.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
+        if(!wordList.contains(endWord)) return 0;
+        Set<String> wordSet = new HashSet<>(wordList);
+
+        Queue<String> queue = new LinkedList<>();
+        queue.offer(beginWord);
+
+        int level = 1;
+
+        while(!queue.isEmpty()){
+            int size = queue.size();
+
+            for(int i = 0; i < size; i++){
+                String curr = queue.poll();
+                char[] chars = curr.toCharArray();
+
+                for(int j = 0; j < chars.length; j++){
+                    char originalChar = chars[j];
+
+                    for(char c = 'a'; c <= 'z'; c++){
+                        chars[j] = c;
+                        String nextWord = new String(chars);
+
+                        if(nextWord.equals(endWord)) return level + 1;
+                        if(wordSet.contains(nextWord)){
+                            queue.offer(nextWord);
+                            wordSet.remove(nextWord);
+                        }
+                    }
+                    chars[j] = originalChar;
+                }
+            }
+            level++;
+        }
+        return 0;
+    }
+}
