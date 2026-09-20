06f18e61becde0345c0e841a8486f533d3c568fe
ASH-CHA <ashton15chavez@gmail.com>
2026-09-20 15:57:47 +0200

    Add: unboundedKnapsack - submission-6

---
+++ Data Structures & Algorithms/unboundedKnapsack/submission-6.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int maximumProfit(List<Integer> profit, List<Integer> weight, int capacity) {
+        int n = profit.size();
+        int[] dp = new int[capacity + 1];
+
+        for(int w = 1; w <= capacity; w++){
+            for(int i = 0; i < n; i++){
+                if(w >= weight.get(i)){
+                    dp[w] = Math.max(dp[w], dp[w - weight.get(i)] + profit.get(i));
+                }
+            }
+        }
+        return dp[capacity];
+    }
+}
